78a7c8ebf1b8333e8c3130c11ccc3bae74116b4e
JuanPabloMoralesG <juan.gomez@quipux.com>
2024-09-02 09:46:19 -0500

    Fix: corregir main para que cumpla la primera entrega

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import uvicorn
 import datetime
-import json
 import logging
 
-from typing import List
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Query
 from fastapi.responses import JSONResponse, PlainTextResponse
 
 app = FastAPI(
@@ -20,9 +18,11 @@ logger = logging.getLogger('uvicorn.error')
          response_description="Un JSON con la fecha actual y la lista ordenada.",
          status_code=status.HTTP_200_OK
          )
-def lista_ordenada(lista_no_ordenada: str) -> JSONResponse:
+def lista_ordenada(lista_no_ordenada: str =
+                   Query(..., alias="lista-no-ordenada")) \
+        -> JSONResponse:
     """
-    Ordena una lista de números enteros proporcionada en formato JSON.
+    Ordena una lista de números enteros proporcionada en formato str.
 
     :param lista_no_ordenada: Una cadena de texto que representa una lista de
     números enteros en formato JSON. Ejemplo: `"[3, 1, 2]"`.
@@ -31,15 +31,17 @@ def lista_ordenada(lista_no_ordenada: str) -> JSONResponse:
     - **400 Bad Request**: Si la entrada no es una lista de enteros válida,
         se devuelve un mensaje de error.
     """
-    try:    
-        lista: List[int] = json.loads(lista_no_ordenada)
+    try:
+        lista_no_ordenada = lista_no_ordenada.strip('[]').split(',')
+        lista_no_ordenada = [int(i) for i in lista_no_ordenada]
+        lista = sorted(lista_no_ordenada)
     except:
         logger.error("No se ingreso una lista de enteros en formato de texto")
         return JSONResponse("Error al convertir la entrada en una lista", 
                             status_code=status.HTTP_400_BAD_REQUEST)
     data = {
         "hora_sistema": str(datetime.datetime.now()),
-        "lista_ordenada": sorted(lista)
+        "lista_ordenada": lista
     }
     return JSONResponse(data, status_code=status.HTTP_200_OK)
 
@@ -64,4 +66,4 @@ if __name__ == '__main__':
     """
     Punto de entrada principal para ejecutar la aplicación con Uvicorn.
     """
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=80, reload=True)
