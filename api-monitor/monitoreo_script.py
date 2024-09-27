866c777daedf9e0a52f7132b5b2b426d7fd03095
JuanPabloMoralesG <juan.gomez@quipux.com>
2024-09-27 21:53:23 +0000

    feat: script para monitorear el api

---
+++ api-monitor/monitoreo_script.py
@@ -0,0 +1,34 @@
+import os
+import time
+import logging
+import requests
+
+# Configuración del host y puerto del contenedor objetivo
+TARGET_CONTAINER_HOST = os.getenv('TARGET_CONTAINER_HOST', 'localhost')
+TARGET_CONTAINER_PORT = os.getenv('TARGET_CONTAINER_PORT', '80')
+CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL',10))
+HEALTHCHECK_ENDPOINT = f'http://{TARGET_CONTAINER_HOST}:{TARGET_CONTAINER_PORT}/healthcheck'
+
+# Configuración del logging
+logging.basicConfig(
+    filename='logs/api-monitor.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def check_health():
+    try:
+        response = requests.get(HEALTHCHECK_ENDPOINT, timeout=5)
+        if response.status_code == 200 and response.text.strip() == 'OK':
+            logging.info(f'Healthcheck passed: {response.status_code} - {response.text}')
+        else:
+            logging.error(f'Healthcheck failed: {response.status_code} - {response.text}')
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Healthcheck request failed: {str(e)}')
+
+if __name__ == "__main__":
+    while True:
+        check_health()
+        time.sleep(CHECK_INTERVAL)
