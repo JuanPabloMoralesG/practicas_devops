99257a9a808e16cfdec57ab673b7496fbd46044c
JuanPabloMoralesG <juan.gomez@quipux.com>
2024-10-02 15:27:17 +0000

    fix: strings de monitoreo

--- api-monitor/monitoreo_script.py
+++ api-monitor/monitoreo_script.py
@@ -22,24 +22,14 @@ def check_health():
     try:
         response = requests.get(HEALTHCHECK_ENDPOINT, timeout=5)
         if response.status_code == 200 and response.text.strip() == 'OK':
-            logging.info(f' Se hizo la solicitud al endpoint 
-                         http://api-container:8000/healthcheck y devolvió {response.text}')
-            print(f' Se hizo la solicitud al endpoint 
-                  http://api-container:8000/healthcheck y devolvió {response.text}')
+            logging.info(f' Se hizo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió {response.text}')
+            print(f' Se hizo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió {response.text}')
         else:
-            logging.error(f'Se hizo la solicitud al endpoint 
-                         http://api-container:8000/healthcheck y devolvió error:
-                          Código de estado: {response.status_code}, Respuesta: {response.text}')
-            print(f'Se hizo la solicitud al endpoint 
-                  http://api-container:8000/healthcheck y devolvió error:
-                  Código de estado: {response.status_code}, Respuesta: {response.text}')
+            logging.error(f'Se hizo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió error:Código de estado: {response.status_code}, Respuesta: {response.text}')
+            print(f'Se hizo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió error:Código de estado: {response.status_code}, Respuesta: {response.text}')
     except requests.exceptions.RequestException as e:
-        logging.error(f'Fallo la solicitud al endpoint 
-                      http://api-container:8000/healthcheck y devolvió error:
-                      Respuesta: {str(e)}')
-        print(f'Fallo la solicitud al endpoint 
-              http://api-container:8000/healthcheck y devolvió error:
-              Respuesta: {str(e)}')
+        logging.error(f'Fallo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió error: Respuesta: {str(e)}')
+        print(f'Fallo la solicitud al endpoint http://api-container:8000/healthcheck y devolvió error: Respuesta: {str(e)}')
 
 if __name__ == "__main__":
     while True:
